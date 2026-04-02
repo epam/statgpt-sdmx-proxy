52393a5d72413245f2fd85d17b941c747aa8f1ca
Iliia Veselov <32986626+iliia-veselov@users.noreply.github.com>
2026-04-02 17:17:44 +0300

    feat: Prepare repo for open source (#6)

--- sdmx-proxy/src/main/java/com/epam/sdmxproxy/registry/configuration/writer/ProxyConfigurationWriter.java
+++
@@ -1,7 +0,0 @@
-package com.epam.sdmxproxy.registry.configuration.writer;
-
-import com.epam.sdmxproxy.configuration.data.ProxyConfiguration;
-
-public interface ProxyConfigurationWriter {
-    void writeConfig(ProxyConfiguration proxyConfiguration);
-}
